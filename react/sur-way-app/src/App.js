8348778740e0e2f8458c7cdfaae22ff96fe17acc
Rohan Sawant <rohansawantct83@gmail.com>
2019-10-05 18:54:04 +0530

    Simple submit form working

--- react/sur-way-app/src/App.js
+++ react/sur-way-app/src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = { username: '' }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleChange(event) {
+    this.setState({ username: event.target.value })
+  }
+
+  handleSubmit(event) {
+    alert(this.state.username)
+    event.preventDefault()
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input
+          type="text"
+          defaultvalue={this.state.username}
+          onChange={this.handleChange}
+        />
+        <input type="submit" value="Submit" />
+      </form>
+    )
+  }
+
 }
 
 export default App;
