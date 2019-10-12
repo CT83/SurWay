982b3c3322f2f93335db062982134b460644cf2f
Rohan Sawant <rohansawantct83@gmail.com>
2019-10-12 22:42:53 +0530

    Center charts on thanks page

--- react/sur-way-app/src/components/ThanksComponent.js
+++ react/sur-way-app/src/components/ThanksComponent.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ChartsComponent from './ChartsComponent';
+import { Grid } from '@material-ui/core';
 
 function ThanksComponent() {
   return (
@@ -7,7 +8,22 @@ function ThanksComponent() {
       <h1>Thanks for completing the Survey!</h1>
       <p>This is what the other users said.</p>
 
-      <ChartsComponent />
+
+      <Grid
+        container
+        spacing={0}
+        direction="column"
+        alignItems="center"
+        justify="center"
+        style={{ minHeight: '100vh' }}
+      >
+
+        <Grid item xs={6}>
+          <ChartsComponent />
+        </Grid>
+
+      </Grid>
+
 
     </React.Fragment>
   )
