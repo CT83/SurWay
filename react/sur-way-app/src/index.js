303cd51c04e44f7d70988bdcc5b70a10c8c1ec2f
Rohan Sawant <rohansawantct83@gmail.com>
2019-11-11 10:01:21 +0530

    Updated theme

--- react/sur-way-app/src/index.js
+++ react/sur-way-app/src/index.js
@@ -2,9 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const theme = createMuiTheme({
+    palette: {
+        primary: { main: "#ff5722" },
+        secondary: {
+            main: '#651fff',
+        },
+    },
+    typography: {
+        useNextVariants: true
+    }
+});
+
+ReactDOM.render(
+    <MuiThemeProvider theme={theme}>
+        <App />
+    </MuiThemeProvider>,
+    document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
